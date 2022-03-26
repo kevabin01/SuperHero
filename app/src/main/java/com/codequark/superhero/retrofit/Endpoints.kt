e00a6256018c2110fee2648768bd95e294481f2a
Kevin Abinady Mejia Flores <kevabin01@gmail.com>
2022-03-26 05:49:19 -0600

    Endpoints

---
+++ app/src/main/java/com/codequark/superhero/retrofit/Endpoints.kt
@@ -0,0 +1,8 @@
+package com.codequark.superhero.retrofit
+
+class Endpoints {
+    companion object {
+        const val baseUrl = "https://superheroapi.com"
+        const val API_KEY = "5070286553030145"
+    }
+}
